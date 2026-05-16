a474df675e6e494e36a41cf1f9b542dfafb75a0e
ytiK0 <tiprson@gmail.com>
2026-05-16 12:01:50 +0300

    feat: add layout styling and routing for the main application structure.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Route, Routes } from "react-router";
+import styles from "./App.module.css"
 function App() {
   return (
     <>
       {/*  Aside menu - navigation */}
       {/*  pages  */}
     </>
+    <div className={styles.appContainer}>
+      <div className={styles.contentContainer}>
+        {/* page content */}
+        <Routes>
+        </Routes>
+      </div>
+    </div>
   )
 }
 
