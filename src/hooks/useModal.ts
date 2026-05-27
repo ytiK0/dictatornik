52e96688d9b91090deda0983104faf4a392aa02a
ytiK0 <tiprson@gmail.com>
2026-05-27 15:41:11 +0300

    feat: add state management for modal open/close status

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -1,18 +1,27 @@
-import {useCallback, useRef} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 
 export function useModal() {
   const dialogRef = useRef<HTMLDialogElement>(null);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const closeModal = useCallback(() => {
-    if (dialogRef.current)
+    if (dialogRef.current) {
+      setIsModalOpen(false)
       dialogRef.current.close();
+    }
   }, []);
 
   const showModal = useCallback(() => {
     if (dialogRef.current) {
+      setIsModalOpen(true)
       dialogRef.current.showModal();
     }
   }, [])
 
-  return [dialogRef, closeModal, showModal] as const;
+  useEffect(() => {
+    if (dialogRef.current)
+      setIsModalOpen(dialogRef.current.open);
+  }, []);
+
+  return [dialogRef, closeModal, showModal, isModalOpen] as const;
 }
