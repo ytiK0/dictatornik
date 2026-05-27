b42679756403b00ce1730a2d74c1e36f9c92bbc0
ytiK0 <tiprson@gmail.com>
2026-05-27 16:09:54 +0300

    refactor: extract aside menu buttons into a constant

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,20 @@ import styles from "./App.module.css"
 import { AsideMenu } from "./components/AsideMenu";
 import {House, Check, ChartColumn} from '@gravity-ui/icons';
 
+const asideButtons = [
+  {label: "Главная", href: "/", icon: <House /> },
+  {label: "Квиз", href: "/quiz", icon: <Check />},
+  {label: "Статистика", href: "/stats", icon: <ChartColumn />}
+]
+
 function App() {
   return (
     <div className={styles.appContainer}>
       <AsideMenu
         className={styles.asideMenuContainer}
-        buttons={[
-          {label: "Главная", href: "/", icon: <House /> },
-          {label: "Квиз", href: "/quiz", icon: <Check />},
-          {label: "Статистика", href: "/stats", icon: <ChartColumn />}
-        ]}
+        buttons={asideButtons}
       />
       <div className={styles.contentContainer}>
-        {/* page content */}
         <Routes>
           <Route index element={<MainPage />} />
           <Route path={"/quiz"} element={<QuizPage />} />
