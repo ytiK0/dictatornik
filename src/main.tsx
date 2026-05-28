c69b868e5ae097bb6471f2da6b18c65538ee98e9
ytiK0 <tiprson@gmail.com>
2026-05-28 11:16:54 +0300

    feat: integrate React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from "react-router";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 import {DbProvider} from "@/context/DbContext";
 import {openDb} from "@/database";
@@ -8,14 +9,18 @@ import App from '@/App.tsx'
 
 import './index.css'
 
-const db = await openDb()
+const db = await openDb();
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <DbProvider db={db}>
-        <App />
-      </DbProvider>
+      <QueryClientProvider client={queryClient} >
+        <DbProvider db={db}>
+          <App />
+        </DbProvider>
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
